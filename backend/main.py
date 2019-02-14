fa3979d737e5c571b28baaba9a0d7005def9cc97
Skyler Knecht <nibbs@Skylers-MacBook-Pro.local>
2019-02-13 20:28:01 -0500

    MAking login method

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,16 @@
-creds = file.open("creds.txt", r)
+creds = open("creds.txt", "r").readlines()
+chars = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+         'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
+         'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}
+
+
+def login():
+    print('Hello Please Login!')
+    username = raw_input("What's your username?: ")
+    for x in creds:
+        usrName = x.split(':')
+        if username is usrName[0]:
+            print(' Wants to login!')
+
+
+login()
